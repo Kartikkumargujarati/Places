f0a278aab139305adf6ff485f44930b3e6c788d3
Kartik Kumar Gujarati <kartikkumar.gujarati@rmn.com>
2019-10-19 12:28:00 -0500

    Initial commit with out of the box setup for Master/Details flow.

---
+++ app/src/androidTest/java/com/kartik/places/ExampleInstrumentedTest.kt
@@ -0,0 +1,29 @@
+/*
+ * Created by Kartik Kumar Gujarati on 10/19/19 12:25 PM
+ * Copyright (c) 2019. All rights reserved.
+ */
+
+package com.kartik.places
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.kartik.places", appContext.packageName)
+    }
+}
